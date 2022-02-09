0a30c49ee85c675d5aa6125fac9d468094051313
Yu Ricky <leoleo5994@gmail.com>
2022-02-09 22:46:37 +0800

    my-portfolio init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <TheNavigation />
+  <div>
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import TheNavigation from "@/components/TheNavigation.vue";
+</script>
