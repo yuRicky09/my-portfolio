9f2c16c67e9abbb32ab7d6765c7483392578c9f9
Yu Ricky <leoleo5994@gmail.com>
2022-02-20 21:51:52 +0800

    contact-pacge樣式與添加google map地圖完成

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>Contact</div>
+  <div
+    class="my-6 grid grid-cols-1 gap-10 lg:my-10 lg:px-28 xl:my-14 xl:h-[calc(100vh-284px)] xl:grid-cols-[1fr_40%] xl:items-center xl:px-0"
+  >
+    <ContactForm />
+    <GoogleMap />
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import ContactForm from "@/components/contact/ContactForm.vue";
+import GoogleMap from "@/components/contact/GoogleMap.vue";
+</script>
