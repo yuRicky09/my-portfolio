a40efd8708d6731caae9af8603efe1cf796acc14
Yu Ricky <leoleo5994@gmail.com>
2022-04-18 22:18:20 +0800

    修正首頁比例縮放造成跑版問題

--- src/components/home/HeroImage.vue
+++ src/components/home/HeroImage.vue
@@ -5,19 +5,13 @@
     <div
       class="absolute top-12 left-0 z-50 hidden font-['Press_Start_2P'] text-violet-500 lg:block"
     >
-      <span ref="textEl" class="text-3xl"></span>
+      <span ref="textEl" class="text-2xl 2xl:text-3xl"></span>
       <span ref="underscoreEl" class="invisible text-3xl font-bold">_</span>
     </div>
 
     <div
       class="hero-image load-hidden relative -mx-4 xs:-mx-0 lg:col-span-1 lg:col-start-2 lg:row-start-1"
     >
-      <div
-        class="absolute top-10 left-2 hidden h-10 w-10 border-t-8 border-l-8 border-violet-500 bg-transparent lg:block"
-      ></div>
-      <div
-        class="absolute right-2 bottom-10 hidden h-10 w-10 border-b-8 border-r-8 border-violet-500 bg-transparent lg:block"
-      ></div>
       <img
         src="../../assets/images/hero-cover.jpeg"
         alt="Yu Ricky Image"
@@ -26,11 +20,11 @@
     </div>
 
     <div class="intro-me load-hidden relative z-10">
-      <div class="text-center lg:text-left xl:text-2xl">
-        <div class="mb-4 flex flex-col gap-2 lg:mb-12">
+      <div class="text-center lg:text-left xl:text-lg">
+        <div class="mb-4 flex flex-col gap-2 lg:mb-8">
           <p>Hi there,👋 I'm</p>
           <h1
-            class="underline decoration-violet-500 decoration-4 underline-offset-2 xl:text-7xl 2xl:text-8xl"
+            class="underline decoration-violet-500 decoration-4 underline-offset-2 xl:text-5xl 2xl:text-8xl"
           >
             Yu Ricky
           </h1>
@@ -65,7 +59,7 @@
             目前致力於使用Vue與TailwindCSS開發個人專案，希望能透過此網站，讓您能更快速地認識我的經歷與使用技術。
           </p>
         </div>
-        <SeeMoreLink class="lg:mt-20 lg:mb-0 lg:justify-start">
+        <SeeMoreLink class="lg:mt-10 lg:mb-0 lg:justify-center xl:mt-14">
           <router-link :to="{ name: 'Projects' }" class="btn"
             >View Projects</router-link
           >
